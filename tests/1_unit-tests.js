5b6918b14e15cf5f624dd9d93a127abbf427ccf6
peetbernard <99289889+peetbernard@users.noreply.github.com>
2023-11-06 07:50:35 +0000

    finalizing project

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,38 +4,44 @@ const ConvertHandler = require("../controllers/convertHandler.js")
 
 let convertHandler = new ConvertHandler()
 
-suite("Unit Tests", function () {
-  test("Whole number", function () {
+suite("Unit Tests", function() {
+  test("Whole number", function(done) {
     const input = "2kg"
     assert.equal(convertHandler.getNum(input), 2)
+    done()
   })
 
-  test("Decimal number", function () {
+  test("Decimal number", function(done) {
     const input = "2.1kg"
     assert.equal(convertHandler.getNum(input), 2.1)
+    done()
   })
 
-  test("Fractional number", function () {
+  test("Fractional number", function(done) {
     const input = "3/2kg"
     assert.equal(convertHandler.getNum(input), 3 / 2)
+    done()
   })
 
-  test("Fractional number with decimal", function () {
+  test("Fractional number with decimal", function(done) {
     const input = "3/2.1kg"
     assert.equal(convertHandler.getNum(input), 3 / 2.1)
+    done()
   })
 
-  test("Double fraction", function () {
+  test("Double fraction", function(done) {
     const input = "3/2/3kg"
     assert.equal(convertHandler.getNum(input), undefined)
+    done()
   })
 
-  test("No numerical input", function () {
+  test("No numerical input", function(done) {
     const input = "kg"
     assert.equal(convertHandler.getNum(input), 1)
+    done()
   })
 
-  test("Each valid input", function () {
+  test("Each valid input", function(done) {
     let input = [
       "gal",
       "l",
@@ -65,25 +71,27 @@ suite("Unit Tests", function () {
       "kg",
     ]
 
-    input.forEach(function (elem, i) {
+    input.forEach(function(elem, i) {
       assert.equal(convertHandler.getUnit(elem), validUnits[i])
     })
+    done()
   })
 
-  test("Invalid input unit", function () {
+  test("Invalid input unit", function(done) {
     const input = "g"
     assert.equal(convertHandler.getUnit(input), undefined)
+    done()
   })
 
-  test("Init-Return unit check", function () {
+  test("Init-Return unit check", function(done) {
     const input = ["gal", "l", "mi", "km", "lbs", "kg"]
     const retour = ["L", "gal", "km", "mi", "kg", "lbs"]
     for (let i = 0; i++; i <= 5) {
       assert.equal(convertHandler.getUnit(input[i]), retour[i])
-    }
+    } done()
   })
 
-  test("Correct spellout", function () {
+  test("Correct spellout", function(done) {
     const validOnes = ["gal", "L", "mi", "km", "lbs", "kg"]
     const spelloutUnits = [
       "liters",
@@ -95,42 +103,48 @@ suite("Unit Tests", function () {
     ]
     for (let i = 0; i++; i <= 5) {
       assert.equal(convertHandler.spellOutUnit(validOnes[i]), spelloutUnits[i])
-    }
+    } done()
   })
 
-  test("gal to L", function () {
+  test("gal to L", function(done) {
     const input = [1, "gal"]
     const result = [3.78541]
-    assert.equal(convertHandler.convert(input[0],input[1]), result[0])
+    assert.equal(convertHandler.convert(input[0], input[1]), result[0])
+    done()
   })
 
-  test("L to gal", function () {
+  test("L to gal", function(done) {
     const input = [1, "L"]
     const result = [0.26417]
-    assert.equal(convertHandler.convert(input[0],input[1]), result[0])
+    assert.equal(convertHandler.convert(input[0], input[1]), result[0])
+    done()
   })
 
-  test("mi to km", function () {
+  test("mi to km", function(done) {
     const input = [1, "mi"]
     const result = [1.60934]
-    assert.equal(convertHandler.convert(input[0],input[1]), result[0])
+    assert.equal(convertHandler.convert(input[0], input[1]), result[0])
+    done()
   })
 
-  test("km to mi", function () {
+  test("km to mi", function(done) {
     const input = [1, "km"]
     const result = [0.62137]
-    assert.equal(convertHandler.convert(input[0],input[1]), result[0])
+    assert.equal(convertHandler.convert(input[0], input[1]), result[0])
+    done()
   })
 
-  test("lbs to kg", function () {
+  test("lbs to kg", function(done) {
     const input = [1, "lbs"]
     const result = [0.45359]
-    assert.equal(convertHandler.convert(input[0],input[1]), result[0])
+    assert.equal(convertHandler.convert(input[0], input[1]), result[0])
+    done()
   })
 
-  test("kg to lbs", function () {
+  test("kg to lbs", function(done) {
     const input = [1, "kg"]
     const result = [2.20462]
-    assert.equal(convertHandler.convert(input[0],input[1]), result[0])
+    assert.equal(convertHandler.convert(input[0], input[1]), result[0])
+    done()
   })
 })
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,7 +6,7 @@ const server = require("../server")
 chai.use(chaiHttp)
 
 suite("Functional Tests", function () {
-  test("/api/convert 10L", function () {
+  test("/api/convert 10L", function (done) {
     chai
       .request(server)
       .get("/api/convert")
@@ -17,9 +17,10 @@ suite("Functional Tests", function () {
         assert.equal(res.body.initUnit, "L")
         assert.approximately(res.body.returnNum, 2.64172, 0.1)
         assert.equal(res.body.returnUnit, "gal")
+        done()
       })
   })
-  test("/api/convert 32g", function () {
+  test("/api/convert 32g", function (done) {
     chai
       .request(server)
       .get("/api/convert")
@@ -27,9 +28,10 @@ suite("Functional Tests", function () {
       .end(function (err, res) {
         assert.equal(res.status, 200)
         assert.equal(res.body.initUnit, undefined)
+        done()
       })
   })
-  test("/api/convert 3/7.2/4kg", function () {
+  test("/api/convert 3/7.2/4kg", function (done) {
     chai
       .request(server)
       .get("/api/convert")
@@ -37,9 +39,10 @@ suite("Functional Tests", function () {
       .end(function (err, res) {
         assert.equal(res.status, 200)
         assert.equal(res.body.initNum, undefined)
+        done()
       })
   })
-  test("/api/convert 3/7.2/4kilomegagram", function () {
+  test("/api/convert 3/7.2/4kilomegagram", function (done) {
     chai
       .request(server)
       .get("/api/convert")
@@ -48,6 +51,8 @@ suite("Functional Tests", function () {
         assert.equal(res.status, 200)
         assert.equal(res.body.initNum, undefined)
         assert.equal(res.body.initUnit, undefined)
+        done()
       })
   })
-})
+}
+     )
